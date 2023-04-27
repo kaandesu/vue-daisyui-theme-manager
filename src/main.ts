443c1bf7cf710788422c3fea8bd669bf66b7f0d2
kaandesu <kaandesu@users.noreply.github.com>
2023-04-27 08:06:30 +0000

    deploy: 6aad0356d1dba334d4ce1dae3c756ca27c59f5ac

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { createThemeManager } from '../src/themeManager'
-
-const app = createApp(App)
-app.use(createThemeManager({ light: 'breeze', dark: 'storm', watchSystemTheme: true }))
-
-app.mount('#app')
--- src/themeManager/types.ts
+++
@@ -1,8 +0,0 @@
-export * from './index'
-export * from './reactives'
-import { useThemeManager } from './index'
-declare module 'vue' {
-  interface globalProperties {
-    $theme: typeof useThemeManager
-  }
-}
